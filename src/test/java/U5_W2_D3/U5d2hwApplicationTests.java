f725256b9ffead6f8f0b919bf4b3498ded27bbb1
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-15 16:58:04 +0200

    up

--- src/test/java/U5_W2_D3/U5d2hwApplicationTests.java
+++ src/test/java/U5_W2_D3/U5d2hwApplicationTests.java
@@ -1,4 +1,4 @@
-package epicode.u5d7hw;
+package U5_W2_D3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class U5d2hwApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
